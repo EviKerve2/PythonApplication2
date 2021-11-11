a96b0936f2dc88eedc457a2c7bd64d78cb515354
opilane <opilane@HP-CZC6297B82>
2021-11-11 09:06:10 +0200

    arithmetic

--- PythonApplication2.py
+++ PythonApplication2.py
@@ -1,1 +1,12 @@
-
+def arithmetic(a, b, oper):
+    if oper=="*":
+        return a*b
+    elif oper=="/":
+        return a/b
+    elif oper=="+":
+        return a+b
+    elif oper=="-":
+        return a-b
+    else:
+        print("Неизвестная операция")
+        return 0
