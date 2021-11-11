4f0c5eb9c40813ba6cc0b5b0a6f13bceec988e8f
opilane <opilane@HP-CZC6297B82>
2021-11-11 09:56:46 +0200

    Практическая работа "Создание пользовательских функций"

--- PythonApplication2.py
+++ PythonApplication2.py
@@ -1,3 +1,4 @@
+#1
 def arithmetic(a, b, oper):
     if oper=="*":
         return a*b
@@ -10,3 +11,43 @@ def arithmetic(a, b, oper):
     else:
         print("Неизвестная операция")
         return 0
+
+#2
+def is_year_leap(year):
+    if (year%4==0) and (year%100!=0) or (year%400==0):
+        return True
+    return False
+
+#3
+import math
+def square(a):
+    P=4*a
+    S=a**2
+    diag=a*math.sqrt(2)
+    return P, S, diag
+
+#4
+def season(month):
+    if month in (12, 1, 2):
+        return "Зима"
+    elif month in (3, 4, 5):
+        return "Весна"
+    elif month in (6, 7, 8):
+        return "Лето"
+    elif month in (9, 10, 11):
+        return "Осень"
+
+#5
+def bank(a, years):
+    for year in range(years):
+        a*=1.1
+    return a
+
+#6
+def is_prime(number):
+    if number==1:
+        return False
+    for possible_divisor in range(2, x):
+        if (number%possible_divisor==0):
+            return False
+    return True
